87c374d68c15cbddd545ede8637ccb2f2e4220ae
Yutao Xu <yutao.xu@intel.com>
2023-04-24 18:22:41 +0800

    Update install_torch.sh

--- install_torch.sh
+++ install_torch.sh
@@ -5,7 +5,6 @@ pip uninstall -y torch
 BASE_DIR=$1
 git config --global --add safe.directory ${BASE_DIR}
 cd ${BASE_DIR}
-git checkout origin/pytorch-1.13.1
 
 pip install -r requirements.txt
 git submodule update --init --recursive
