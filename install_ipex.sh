c195a53e612c46a8e8432d2100320b35dfce0523
Yutao Xu <yutao.xu@intel.com>
2023-04-24 18:25:44 +0800

    Update install_ipex.sh

--- install_ipex.sh
+++ install_ipex.sh
@@ -5,7 +5,7 @@ pip uninstall -y ipex
 pip uninstall -y intel_extension_for_pytorch
 
 BASE_DIR=$1
-git config --global --add safe.directory ${BASE_DIR}
+git config --global --add safe.directory '*'
 cd ${BASE_DIR}
 
 pip install -r requirements.txt
