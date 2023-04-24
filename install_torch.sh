e02eb36e16b421f9461e8673e7f61513fea73fdd
Yutao Xu <yutao.xu@intel.com>
2023-04-24 18:12:30 +0800

    Update install_torch.sh

--- install_torch.sh
+++ install_torch.sh
@@ -1,12 +1,9 @@
 read -p "install (y/n)? " CONT
 if [ "$CONT" = "y" ]; then
-
-rm -rf frameworks.ai.pytorch.private-gpu
-
 pip uninstall -y torch
-git clone https://github.com/intel-innersource/frameworks.ai.pytorch.private-gpu.git
-cd frameworks.ai.pytorch.private-gpu
 
+BASE_DIR=$1
+cd ${BASE_DIR}
 git checkout origin/pytorch-1.13.1
 
 pip install -r requirements.txt
