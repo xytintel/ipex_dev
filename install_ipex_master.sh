c678fab82ea94011429f08a21f36c574115dde90
Yutao Xu <yutao.xu@intel.com>
2023-04-24 18:12:16 +0800

    Update install_ipex_master.sh

--- install_ipex_master.sh
+++ install_ipex_master.sh
@@ -1,23 +1,12 @@
 read -p "install (y/n)? " CONT
 if [ "$CONT" = "y" ]; then
-
-########## Note:!!!!!! ##########
-BASE_DIR=ipex-master
-#################################
-
-rm -rf frameworks.ai.pytorch.ipex-gpu
-rm -rf ${BASE_DIR}
-
 pip uninstall -y torch_ipex
 pip uninstall -y ipex
 pip uninstall -y intel_extension_for_pytorch
-git clone https://github.com/intel-innersource/frameworks.ai.pytorch.ipex-gpu.git
-mv frameworks.ai.pytorch.ipex-gpu ${BASE_DIR}
-cd ${BASE_DIR}
 
-########## Note:!!!!!! ##########
+BASE_DIR=$1
+cd ${BASE_DIR}
 git checkout origin/master
-#################################
 
 pip install -r requirements.txt
 git submodule update --init --recursive
